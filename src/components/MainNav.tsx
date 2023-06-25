c309662597ce7fc52e4fe29657a876546fae95d6
kevinsmasters <gmail@kevinmasters.com>
2023-06-25 13:39:22 -0400

    add main app container, hero component, start nav component

---
+++ src/components/MainNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MainNav = () => {
+  return (
+    <div id="mainNav">
+      <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark-nu">
+        <div className="container"></div>
+      </nav>
+    </div>
+  );
+};
